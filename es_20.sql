13e3b736e5d08b6802ea8f815324c2b75de6747f
alberto-taccon <97546990+alberto-taccon@users.noreply.github.com>
2026-03-05 09:37:12 +0100

    Update es_20.sql

--- es_20.sql
+++ es_20.sql
@@ -10,7 +10,20 @@ Si richiede di modificare il modello relazionale introducendo le nuove strutture
 necessarie, mantenendo la coerenza con le entità già definite (Treno, Tratta, Corsa).
 
 CREATE TABLE stazione (
-    id VARCHAR(10),
-    nome VARCHAR(33),
+    id_stazione INT PRIMARY KEY NOT NULL,
+    nome_stazione VARCHAR(33),
     citta VARCHAR(33)
 );
+
+fermata non funziona
+CREATE TABLE fermata (
+    id_fermata INT PRIMARY KEY NOT NULL,
+    id_stazione INT,
+    FOREIGN KEY (id_stazione) REFERENCES stazione(id_stazione),
+    id_tratta INT AUTO_INCREMENT NOT NULL, 
+    FOREIGN KEY (id_tratta) REFERENCES tratta(id_tratta),
+    ordine_progressivo INT NOT NULL
+);
+
+ALTER TABLE tratta DROP COLUMN stazione_partenza
+ALTER TABLE tratta DROP COLUMN stazione_arrivo;
