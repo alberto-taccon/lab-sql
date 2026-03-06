c7449c83b4da51fbee4d61788b00e1fcceabe543
alberto-taccon <97546990+alberto-taccon@users.noreply.github.com>
2026-03-06 18:18:43 +0100

    Update gest_libri.sql

--- gest_libri.sql
+++ gest_libri.sql
@@ -1,16 +1,17 @@
 CREATE TABLE libro(
-    id_libro INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    isbn INT(13) NOT NULL,
+    id_libro INT PRIMARY KEY AUTO_INCREMENT,
+    isbn VARCHAR(13) NOT NULL,
     tipologia VARCHAR(30) NOT NULL,
-    casa_editrice VARCHAR(30) NOT NULL,
-    FOREIGN KEY (casa_editrice) REFERENCES case_editrice(nome_casa_editrice),
+    id_casa_editrice INT NOT NULL,
     numero_edizione INT NOT NULL,
     prezzo FLOAT NOT NULL,
-    anno YEAR
+    anno YEAR,
+    FOREIGN KEY (id_autore) REFERENCES autore(id_autore),
+    FOREIGN KEY (id_casa_editrice) REFERENCES casa_editrice(id_casa_editrice)
 );
 
 CREATE TABLE autore(
-    id_autore INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_autore INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     cognome VARCHAR(30) NOT NULL,
     data_nascita DATE,
@@ -19,9 +20,16 @@ CREATE TABLE autore(
 );
 
 CREATE TABLE casa_editrice(
-    id_casa_editrice INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_casa_editrice INT PRIMARY KEY AUTO_INCREMENT,
     nome_casa_editrice VARCHAR(30) NOT NULL,
     nazione VARCHAR(30) NOT NULL,
     anno_fondazione YEAR,
     pubblica BOOLEAN
   );
+
+CREATE TABLE libro_autore(
+    id_libro INT PRIMARY KEY,
+    id_autore INT PRIMARY KEY,
+    FOREIGN KEY (id_libro) REFERENCES libro(id_libro),
+    FOREIGN KEY (id_autore) REFERENCES autore(id_autore)
+);
